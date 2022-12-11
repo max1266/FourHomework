acc72b8f2d651998eeae54d149747c6de67094a3
Maxim_shmidt <history1266@mail.ru>
2022-12-11 11:49:23 +0700

    задача 25

---
+++ task25/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("введите первое число");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("введите второе число");
+double b = Convert.ToDouble(Console.ReadLine());
+double i = 0;
+double result = 1;
+while(i < b)
+{
+    result = result * a;
+    i++;
+}
+Console.WriteLine(result);
