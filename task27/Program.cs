43be3a5951a1af17a21c219eb6f70e9b55990725
Maxim_shmidt <history1266@mail.ru>
2022-12-12 12:30:59 +0700

    задание 27

--- task27/Program.cs
+++ task27/Program.cs
@@ -1,8 +1,12 @@
 ﻿Console.Clear();
-string[] a = Console.ReadLine();
-int count = a.Length;
-int i = 0;
-int b = int.Parse(a[i]);
-Console.WriteLine(b);
+int a = Convert.ToInt32(Console.ReadLine());
+int resultt = 0;
+while(a >10 )
+{
+    resultt = resultt + (a%10);
+    a = a/10;
+}
+resultt = resultt + a;
+Console.WriteLine(resultt);
 
 
