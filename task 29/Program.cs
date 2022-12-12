422737ddec6e17e2e94b7b91ab82fb71aa452eac
Maxim_shmidt <history1266@mail.ru>
2022-12-12 12:09:55 +0700

    задача 29

---
+++ task 29/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[a];
+for(int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"Введите {i+1} число");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"[{String.Join(", ", array)}]");
---
+++ task27/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+string[] a = Console.ReadLine();
+int count = a.Length;
+int i = 0;
+int b = int.Parse(a[i]);
+Console.WriteLine(b);
+
+
